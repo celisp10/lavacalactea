02b5fde5c880194b31a9798ec933831a36329cbd
Esteban celis <celispuertae@gmail.com>
2024-08-05 18:38:35 -0500

    V1.1.1 - Se agregó un log in con núevos estilo y se configuró la base de datos. Tambien se corrigio <newUser> agregandole núevos datos a ingresar

--- configs/database.php
+++ configs/database.php
@@ -1,5 +1,19 @@
 <?php
 
-// class {
-    
-// }
+class connectionDB{
+    private $server = 'localhost';
+    private $dbname = 'la_vaca_lactea_db'; 
+    private $user = 'root';
+    private $dbpassword = '';
+    private $pdo;
+
+    public function __construct() {
+        try {
+            $this->pdo = new PDO("mysql:host=$this->server;dbname=$this->dbname", $this->user, $this->dbpassword);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $this->pdo;
+        } catch(PDOException $e) {
+            echo 'Error en la conexión a la base de datos: '.$e->getMessage();
+        }
+    }
+}
